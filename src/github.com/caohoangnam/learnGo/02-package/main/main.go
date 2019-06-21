d8f4a37f27335fb5da42134e2e4d3108ee8a41b4
CaoNam <hoangnam@lozi.vn>
2019-06-22 00:45:33 +0700

    UPDATE: move file github

---
+++ src/github.com/caohoangnam/learnGo/02-package/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/caohoangnam/learnGo/02-package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+}
